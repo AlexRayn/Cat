913a8c15c6f37897381f03ba3b16ef04508a1dca
AlexRayn <120960034+AlexRayn@users.noreply.github.com>
2023-05-13 00:44:59 +0200

    Learned how to create class objects and call object methods.

--- src/Loader.java
+++ src/Loader.java
@@ -4,5 +4,12 @@ public class Loader
     public static void main(String[] args)
     {
         Cat murka = new Cat();
+        System.out.println(murka.getWeight());
+        murka.meow(2000.0);
+        System.out.println(murka.getWeight());
+        System.out.println("Мурка " + murka.getStatus());
+        Cat boby = new Cat();
+        boby.feed(10000.0);
+        System.out.println("Боби " + boby.getStatus());
     }
 }
